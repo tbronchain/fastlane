4ea78d3fbca4e936faeb39756b5ce2f973991331
Guillaume Verbal <doweig@gmail.com>
2012-12-02 12:34:10 +0100

    up cashier

--- assets/nowaist.js
+++ assets/nowaist.js
@@ -65,8 +65,8 @@ $(document).ready(function(){
 	var i = 0;
 	var currentPos = 1;
 	var list = '';
-	var getListUrl = '/API/api.php?mode=get_list';
-	var validateClientUrl = '/API/api.php?mode=validate_client';
+	var getListUrl = './API/api.php?mode=get_list';
+	var validateClientUrl = './API/api.php?mode=validate_client';
 	$.ajax({
 		url: getListUrl,
 		success: function(data){
