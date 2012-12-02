2a9eff7b91b186275938407bd098c3d5e1ee31b0
Guillaume Verbal <doweig@gmail.com>
2012-12-02 12:36:34 +0100

    up cashier

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<link media="screen" rel="stylesheet" type="text/css" href="/assets/bootstrap/css/bootstrap.min.css">
-	<link media="screen" rel="stylesheet" type="text/css" href="/assets/index.css">
+	<link media="screen" rel="stylesheet" type="text/css" href="./assets/bootstrap/css/bootstrap.min.css">
+	<link media="screen" rel="stylesheet" type="text/css" href="./assets/index.css">
     
     <meta http-equiv="content-type" content="text/php; charset=utf-8" />
 
@@ -13,7 +13,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="logo">
-				<img src="/assets/index.png" />
+				<img src="./assets/index.png" />
 				<a href="cashier.php" class="btn btn-large btn-primary btn-next">Try it !</a>
 			</div>
         </div>
